36cb40812d8355ef8539d5332c9a6691d457a701
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-04-07 06:52:12 +0530

    Complete templates - chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app # importing from app package the app variable
 
 @app.route('/') # decorators : modify the function
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  user = {'username': 'Greenal'}
+  posts = [
+    {
+      'author': {'username': 'John'},
+      'body': 'Beautiful day in Portland!'
+    }, 
+    {
+      'author': {'username': 'Susan'},
+      'body': 'The Avengers movie was so cool!'
+    }
+  ]
+  return render_template('index.html', title='Home', user=user, posts=posts)
